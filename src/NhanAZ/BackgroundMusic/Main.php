93ee3140f8738b5e38df4e29c1a2754cbf77438c
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2023-07-23 17:10:19 +0700

    Solve #2

--- src/NhanAZ/BackgroundMusic/Main.php
+++ src/NhanAZ/BackgroundMusic/Main.php
@@ -4,43 +4,34 @@ declare(strict_types=1);
 
 namespace NhanAZ\BackgroundMusic;
 
+use NhanAZ\libBedrock\ResourcePackManager;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\ClosureTask;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\network\mcpe\protocol\PlaySoundPacket;
-use NhanAZ\libRegRsp\libRegRsp;
 
 class Main extends PluginBase implements Listener {
 
-	private libRegRsp $libRegRsp;
-
 	protected function onEnable(): void {
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->libRegRsp = new libRegRsp($this);
-		$this->libRegRsp->regRsp();
+		ResourcePackManager::registerResourcePack($this);
 	}
 
 	protected function onDisable(): void {
-		$this->libRegRsp->unregRsp();
+		ResourcePackManager::unRegisterResourcePack($this);
 	}
 
 	public function onJoin(PlayerJoinEvent $event): void {
 		$player = $event->getPlayer();
 		$playerPos = $player->getPosition();
 		$packet = PlaySoundPacket::create(
-			/** string $soundName */
-			"C418Sweden",
-			/** float $x */
-			$playerPos->getX(),
-			/** float $y */
-			$playerPos->getY(),
-			/** float $z */
-			$playerPos->getZ(),
-			/** float $volume */
-			1.0,
-			/** float $pitch */
-			1.0
+			soundName: "C418Sweden",
+			x: $playerPos->getX(),
+			y: $playerPos->getY(),
+			z: $playerPos->getZ(),
+			volume: 1.0,
+			pitch: 1.0
 		);
 		$this->getScheduler()->scheduleRepeatingTask(new ClosureTask(function () use ($player, $packet): void {
 			if ($player->isOnline()) {
